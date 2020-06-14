9b9dd9e76c3b2cfc9e26c4091876d93579f6b7b1
Michael Horrell <michaelhorrell@michael-macbook.lan>
2020-06-14 15:15:47 -0500

    verified numbers and results are same

--- wideboost/objectives/categoricallogloss.py
+++ wideboost/objectives/categoricallogloss.py
@@ -1,32 +1,6 @@
 import numpy as np
 
 def categoricallogloss_gradient_hessian(X,B,Y):
-    ## TODO: Fix so that it is softmax(X - max(x)) for numerical stability
-    
-    Y = Y.reshape([Y.shape[0],-1])
-    
-    assert len(X.shape) == 2
-    assert len(B.shape) == 2
-    assert len(Y.shape) == 2
-
-    logits = X.dot(B)
-
-    Z = np.exp(X.dot(B))
-
-    ZtBi = Z.dot(B.transpose())
-    ZtBi2 = Z.dot(np.square(B.transpose()))
-    
-    Zt1 = np.tile(np.sum(Z,axis=1,keepdims=True),[1,B.shape[0]])
-
-    dX =  ZtBi/Zt1 - B[:,Y.astype(int)].transpose()
-    dX2 = ZtBi2 / Zt1 - np.square(ZtBi / Zt1)
-
-    return dX, dX2
-
-
-
-def cgh(X,B,Y):
-    ## TODO: Verify the numbers are right
     Y = Y.reshape([Y.shape[0],-1])
     
     def _onehot(Y):
