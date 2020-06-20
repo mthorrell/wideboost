b81634b1c086ce6e7e6281523acb85541a2b4d5c
Michael Horrell <michaelhorrell@michael-macbook.lan>
2020-06-19 21:15:00 -0500

    added examples and sklearn functionality

--- wideboost/objectives/categoricallogloss.py
+++ wideboost/objectives/categoricallogloss.py
@@ -11,16 +11,16 @@ def categoricallogloss_gradient_hessian(X,B,Y):
     assert len(X.shape) == 2
     assert len(B.shape) == 2
     assert len(Y.shape) == 2
-
+    
     logits = X.dot(B)
     max_logit = np.max(logits,axis=1,keepdims=True)
     logits = logits - max_logit
-
+    
     sum_exp = np.sum(np.exp(logits),axis=1,keepdims=True)
     P = np.exp(logits) / sum_exp
-
+    
     eps = 1e-16
     dX = (P - _onehot(Y)).dot(B.transpose())
     dX2 = np.maximum(P.dot(np.square(B).transpose()) - np.square(P.dot(B.transpose())),eps)
-
+    
     return dX, dX2
