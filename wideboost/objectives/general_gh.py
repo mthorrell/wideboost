fdc63c87830ef7ea9a4246814b86598a560f57e5
Michael Horrell <michaelhorrell@michael-macbook.lan>
2020-09-07 21:01:57 -0500

    deleted commented code

--- wideboost/objectives/general_gh.py
+++ wideboost/objectives/general_gh.py
@@ -22,39 +22,3 @@ def row_diag(M):
     diag = np.arange(M.shape[1])
     b[:, diag, diag] = M
     return b
-
-
-# from wideboost.objectives.squareloss import squareloss_gradient_hessian as sgh
-# from wideboost.objectives.categoricallogloss import categoricallogloss_gradient_hessian as cgh
-# import numpy as np
-
-# Y = np.argmax(np.random.multinomial(1,[0.3,0.3,0.4],size = 10),axis=1)
-# F = np.random.random([10,4])
-# B = np.random.random([4,3])
-
-# GH = cgh(F,B,Y)
-
-# logits = F.dot(B)
-# max_logit = np.max(logits,axis=1,keepdims=True)
-# logits = logits - max_logit
-# sum_exp = np.sum(np.exp(logits),axis=1,keepdims=True)
-# P = np.exp(logits)/sum_exp
-
-# gradient = P - _onehot(Y)
-# gB = f_gradient_B(gradient,B)
-# print(np.abs(gB - GH[0]))
-
-# H = row_diag(P)
-# rP = P.reshape([-1,3,1])
-# rPT = P.reshape([-1,1,3])
-# H = H - np.matmul(rP,rPT)
-
-# hB = f_hessian_B(H,B)
-# print(np.abs(hB - GH[1]))
-# print(np.max(np.abs(hB-GH[1])))
-
-# def row_diag(M):
-#     b = np.zeros((M.shape[0], M.shape[1], M.shape[1]))
-#     diag = np.arange(M.shape[1])
-#     b[:, diag, diag] = M
-#     return b
