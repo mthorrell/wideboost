3e486e8daac22fb77335a458010fc4de74d29b74
mthorrell <mthorrell@github.com>
2022-11-20 15:22:23 -0600

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wideboost",
-    version="0.4.0",
+    version="0.4.1",
     author="Michael Horrell",
     author_email="mthorrell@github.com",
     description="Implements Wide Boosting functions for popular boosting packages",
